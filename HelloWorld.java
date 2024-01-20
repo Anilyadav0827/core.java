39033abcdd50321653d7ef1e173eccb85f4f4bef
Anilyadav0827 <anilyadavmekala6@gmail.com>
2024-01-20 15:55:07 +0530

    Added Neoteric added core java added For loop

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,8 @@ public class HelloWorld {
         System.out.println("hello World");
         System.out.println("Hello Neoteric ");
         System.out.println("core Java");
+        for (int i=0;i<=5;i++){
+            System.out.println("welcome to Neoteric Method"+" "+i);
+        }
     }
 }
